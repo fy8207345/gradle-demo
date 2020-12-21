dd23c6704f6adb74a6fd2bc90c82312c3b956db7
administrator <2316696377@qq.com>
2020-12-21 16:14:07 +0800

    init

---
+++ child/src/main/java/com/fy/child/ChildService.java
@@ -0,0 +1,8 @@
+package com.fy.child;
+
+public class ChildService {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ child2/src/main/java/com/fy/child2/Child2Service.java
@@ -0,0 +1,8 @@
+package com.fy.child2;
+
+public class Child2Service {
+
+    public static final String invoke(){
+        return "child2";
+    }
+}
